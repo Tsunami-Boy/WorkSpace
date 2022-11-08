9a4577cfe8255e96ea475b06a7d23295783404b6
Benjamin Daza <benja.daza.jimenez@gmail.com>
2022-11-08 19:47:47 -0300

    Subir archivo

---
+++ Tarea.scm
@@ -0,0 +1,37 @@
+#lang racket
+(define esto-es-una-definicion 24)
+;Esto es un comentario
+#|
+Esto es un comentario multi linea
+|#
+(if #f A B);Retorna A si es true y B si es false
+(exact? parametro);retorna true or false
+(inexact? parametro);retorna true or false
+;---------------------------------------------------------------------------------
+
+;Aqui empieza el codigo
+(define (inverso lista n)
+  ;Realizar el codigo
+  )
+
+(define (umbral_simple lista umbral tipon)
+  ;Realizar el codigo
+  )
+(define (umbral_cola lista umbral tipo)
+  ;Realizar el codigo
+  )
+
+(define (modsel_simple lista seleccion f)
+  ;Realizar el codigo
+  )
+(define (modsel_cola lista seleccion f)
+  ;Realizar el codigo
+  )
+
+(define (estables lista umbral fM fm)
+  ;Realizar el codigo
+  )
+
+(define (query lista pos op params)
+  ;Reaalizar el codigo
+  )
