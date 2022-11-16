6a05683350901665e6b196fff397e44debd92f26
Benjamin Daza <benja.daza.jimenez@gmail.com>
2022-11-16 16:41:34 -0300

    actu

--- Tarea.scm
+++ Tarea.scm
@@ -1,28 +1,23 @@
 #lang racket
 #|
-
 (define (inverso lista n)
-  (length lista)
-  ;crear la lista de numeros hasta n-1 (list)
-  ;recorrer la lista y recuperar numero por numero (num)
-  if((andmap igual-igual num list) "Sacar el numero" "Mantener el numero")
-  ;Realizar el codigo
+  (inverso_ lista n (list ))
   )
 
-(define (igual-igual num list) (not(boolean? (member num list))))
-
-(define (lista-factorial n list)
-  (if(= n 0)(append list 0)
-     (append list (- n 1))))
-
-(define (factorial n)
-  (if (= n 0) 1 (* n (factorial(- n 1)))))
-(define (inverso lista n)
+(define (inverso_ lista  n lista_)
+  ;Condicion de que si la lista es mayor estricta que 1 se hace el primer lenth, si es falso hacer las otras 2
   (cond
-    []
-    []
-    []
-    []))
+    [(>= (length(rest lista)) 1) (inverso_ (rest lista) n (Aux lista_ (car lista) (if (= (length lista_) 0) 0 (last lista_))))]
+    [(= (length(rest lista)) 0) (inverso_ (list ) n (Aux lista_ (car lista) (if (= (length lista_) 0) 0 (last lista_))))]
+    [(= (length lista) 0) (Aux lista_ n (if (= (length lista_) 0) 0 (last lista_)))]
+    ))
+
+(define (Aux lista_ num i) 
+  (cond
+    [(< i num) (Aux (append lista_ (list i)) num (+ i 1))]
+    [(= i num) (append empty lista_)]
+    ))
+(inverso '(1 3 5 7) 8)
 |#
 ;-----------------------------------------------------------------------------
 #|
