306a51c579ebeaf3737185524ec8a59f7e2897d8
Benja <benja.daza.jimenez@gmail.com>
2022-11-16 20:41:27 -0300

    actua

--- Tarea.scm
+++ Tarea.scm
@@ -1,24 +1,20 @@
 #lang racket
-#|
+
 (define (inverso lista n)
-  (inverso_ lista n (list ))
+  (inverso_ lista n (list ) 0 0)
   )
 
-(define (inverso_ lista  n lista_)
-  ;Condicion de que si la lista es mayor estricta que 1 se hace el primer lenth, si es falso hacer las otras 2
-  (cond
-    [(>= (length(rest lista)) 1) (inverso_ (rest lista) n (Aux lista_ (car lista) (if (= (length lista_) 0) 0 (last lista_))))]
-    [(= (length(rest lista)) 0) (inverso_ (list ) n (Aux lista_ (car lista) (if (= (length lista_) 0) 0 (last lista_))))]
-    [(= (length lista) 0) (Aux lista_ n (if (= (length lista_) 0) 0 (last lista_)))]
-    ))
+(define (inverso_ lista  n lista_ i j)
+  (if (> (length lista) 1)
+      (inverso_ (rest lista) n (Aux lista_ (car lista) (if (= j 0) 0 (+ i 1))) (car lista) (+ j 1))
+      (if (= (length lista) 1)
+          (inverso_ (list ) n (Aux lista_ (car lista) (if (= j 0) 0 (+ i 1))) (car lista) (+ j 1))
+          (Aux lista_ n (if (= j 0) 0 (+ i 1))))))
 
 (define (Aux lista_ num i) 
   (cond
-    [(< i num) (Aux (append lista_ (list i)) num (+ i 1))]
     [(= i num) (append empty lista_)]
-    ))
-(inverso '(1 3 5 7) 8)
-|#
+    [(< i num) (Aux (append lista_ (list i)) num (+ i 1))]))
 ;-----------------------------------------------------------------------------
 #|
 (define (umbral_simple lista umbral tipon)
