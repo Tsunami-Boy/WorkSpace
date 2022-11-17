32cfa01fbc8d3ab083b4e8554c5b7f24469bdeee
Benja <benja.daza.jimenez@gmail.com>
2022-11-16 21:20:39 -0300

    actu

--- Tarea.scm
+++ Tarea.scm
@@ -1,5 +1,4 @@
 #lang racket
-
 (define (inverso lista n)
   (inverso_ lista n (list ) 0 0)
   )
@@ -15,11 +14,36 @@
   (cond
     [(= i num) (append empty lista_)]
     [(< i num) (Aux (append lista_ (list i)) num (+ i 1))]))
-;-----------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------------------------
+
+(define (umbral_simple lista umbral tipo)
+  (if (equal? tipo #\M)
+      (mayor_simple lista umbral 0 (list ))
+      (menor_simple lista umbral 0 (list ))))
+
+(define (mayor_simple lista umbral i lista_)
+  (if (equal? empty lista)
+      (append lista empty)
+      (if (> (length lista) 1)
+          (if (> (car lista) umbral)
+              (mayor_simple (rest lista) umbral (+ i 1) (append lista_ (list i)))
+              (mayor_simple (rest lista) umbral (+ i 1) lista_))
+          (if (> (car lista) umbral)
+              (append lista_ (list i))
+              (append lista_ empty)))))
+
+(define (menor_simple lista umbral i lista_)
+  (if (equal? empty lista)
+      (append lista empty)
+      (if (> (length lista) 1)
+          (if (< (car lista) umbral)
+              (menor_simple (rest lista) umbral (+ i 1) (append lista_ (list i)))
+              (menor_simple (rest lista) umbral (+ i 1) lista_))
+          (if (< (car lista) umbral)
+              (append lista_ (list i))
+              (append lista_ empty)))))
+;----------------------------------------------------------------------------------------------------------
 #|
-(define (umbral_simple lista umbral tipon)
-  ;Realizar el codigo
-  )
 (define (umbral_cola lista umbral tipo)
   ;Realizar el codigo
   )
