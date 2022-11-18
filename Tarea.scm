efc13691cfbd72ff3dd2e38d71bfa58c56ad0023
Benja <benja.daza.jimenez@gmail.com>
2022-11-18 20:09:48 -0300

    ACtu

--- Tarea.scm
+++ Tarea.scm
@@ -20,7 +20,7 @@
 ;(inverso '() 10)
 ;----------------------------------------------------------------------------------------------------------
 
-(define (umbral_simple lista umbral tipo)
+(define (umbral_cola lista umbral tipo)
   (if (equal? tipo #\M)
       (mayor_simple lista umbral 0 (list ))
       (menor_simple lista umbral 0 (list ))))
@@ -47,26 +47,28 @@
               (append lista_ (list i))
               (append lista_ empty)))))
 
-;(umbral_simple '(15 2 1 3 27 5 10) 5 #\M)
-;(umbral_simple '(15 2 1 3 27 5 10) 5 #\m)
+;(umbral_cola '(15 2 1 3 27 5 10) 5 #\M)
+
 ;----------------------------------------------------------------------------------------------------------
 #|
-(define (umbral_cola lista umbral tipo)
+(define (umbral_simple lista umbral tipo)
   ;Realizar el codigo
   )
 |#
+
+;(umbral_simple '(15 2 1 3 27 5 10) 5 #\m)
 ;----------------------------------------------------------------------------------------------------------
-(define (modsel_simple lista seleccion f)
-  (modsel_simple_ lista seleccion f (list ) 0)
+(define (modsel_cola lista seleccion f)
+  (modsel_cola_ lista seleccion f (list ) 0)
   )
 
-(define (modsel_simple_ lista seleccion f lista_ i)
+(define (modsel_cola_ lista seleccion f lista_ i)
   (if (equal? empty lista)
       (append lista_ empty)
       (if (> (length lista) 1)
           (if (confirmar seleccion i)
-              (modsel_simple_ (rest lista) seleccion f (append lista_ (list (f (car lista)))) (+ i 1))
-              (modsel_simple_ (rest lista) seleccion f (append lista_ (list (car lista))) (+ i 1)))
+              (modsel_cola_ (rest lista) seleccion f (append lista_ (list (f (car lista)))) (+ i 1))
+              (modsel_cola_ (rest lista) seleccion f (append lista_ (list (car lista))) (+ i 1)))
           (if (confirmar seleccion i)
               (append lista_ (list (f (car lista))))
               (append lista_ (list (car lista)))))))
@@ -74,11 +76,11 @@
 (define (confirmar lista num)
   (not (boolean? (member num lista))))
 
-;(modsel_simple '(15 2 1 3 27 5 10) '(0 4 6) (lambda (x) (modulo x 2)))
-;(modsel_simple '(15 2 1 3 27 5 10) '(3 1 2) (lambda (x) (+ x 5)))
+;(modsel_cola '(15 2 1 3 27 5 10) '(0 4 6) (lambda (x) (modulo x 2)))
+;(modsel_cola '(15 2 1 3 27 5 10) '(3 1 2) (lambda (x) (+ x 5)))
 ;----------------------------------------------------------------------------------------------------------
 #|
-(define (modsel_cola lista seleccion f)
+(define (modsel_simple lista seleccion f)
   ;Realizar el codigo
   )
 |#
@@ -89,32 +91,32 @@
 (define (estables_FM lista umbral fM);fM
   (if (equal? lista empty)
       (0)
-      (length (umbral_simple
-               (modsel_simple lista (umbral_simple lista umbral #\M) fM)
+      (length (umbral_cola
+               (modsel_cola lista (umbral_cola lista umbral #\M) fM)
                umbral
                #\M))))
 
 (define (estables_Fm lista umbral fm);fm
   (if (equal? lista empty)
       0
-      (length (umbral_simple
-               (modsel_simple lista (umbral_simple lista umbral #\m) fm)
+      (length (umbral_cola
+               (modsel_cola lista (umbral_cola lista umbral #\m) fm)
                umbral
                #\m))))
 #|
 (define (estables_fM lista umbral fM)
   (if (equal? lista empty)
       0
-      (length (umbral_simple
-               (aplicar fM (lista_val lista (umbral_simple lista umbral #\M) (list ) 0) (list )); Aplicarles f
+      (length (umbral_cola
+               (aplicar fM (lista_val lista (umbral_cola lista umbral #\M) (list ) 0) (list )); Aplicarles f
                umbral
                #\M))))
 
 (define (estables_fm lista umbral fm)
   (if (equal? lista empty)
       0
-      (length (umbral_simple
-               (aplicar fm (lista_val lista (umbral_simple lista umbral #\m) (list ) 0) (list )); Aplicarles f
+      (length (umbral_cola
+               (aplicar fm (lista_val lista (umbral_cola lista umbral #\m) (list ) 0) (list )); Aplicarles f
                umbral
                #\m))))
 
@@ -136,10 +138,10 @@
           (append lista_ (list (car lista1)))
           (append lista_ empty))))
 
-;(lista_val '(15 2 1 3 27 5 10) (umbral_simple '(15 2 1 3 27 5 10) 5 #\m) (list ) 0) ;Funciona
+;(lista_val '(15 2 1 3 27 5 10) (umbral_cola '(15 2 1 3 27 5 10) 5 #\m) (list ) 0) ;Funciona
 |#
 
-(estables '(15 2 1 3 27 5 10) 5 (lambda (x) (/ x 2)) (lambda (x) (* x 2)))
+;(estables '(15 2 1 3 27 5 10) 5 (lambda (x) (/ x 2)) (lambda (x) (* x 2)))
 ;----------------------------------------------------------------------------------------------------------
 
 (define (query lista pos op params)
@@ -154,10 +156,10 @@
       (query_ (rest lista) pos op params (+ i 1))))
 
 (define (query_1 lista params);Funciona
-  (umbral_simple lista (car params) (last params)))
+  (umbral_cola lista (car params) (last params)))
 
 (define (query_2 lista params)
-  (modsel_simple lista (car params) (eval (last params))))
+  (modsel_cola lista (car params) (eval (last params))))
 
 (define (query_3 lista params)
   (estables lista (car params) (eval (car (rest params))) (eval (last params))))
