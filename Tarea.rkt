6943299f99778e0827b4fcd571816e5c0fae8655
Benja <benja.daza.jimenez@gmail.com>
2022-11-20 19:43:22 -0300

    New

--- Tarea.rkt
+++ Tarea.rkt
@@ -1,4 +1,4 @@
-#lang racket (current-namespace
+#lang scheme (current-namespace
 (make-base-namespace))
 
 (define (inverso lista n)
@@ -29,7 +29,7 @@
     [(= i num) (append empty lista_)]
     [(< i num) (Aux (append lista_ (list i)) num (+ i 1))]))
 
-;(inverso '() 10)
+;(inverso '(1 3 7) 10)
 ;----------------------------------------------------------------------------------------------------------
 
 (define (umbral_cola lista umbral tipo)
