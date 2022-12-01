91676816619fc37aaa9e486889aa2f5e9bfbb03c
Benjamin Daza <benja.daza.jimenez@gmail.com>
2022-12-01 18:45:29 -0300

    Actu

--- Tarea_prolog.pl
+++ Tarea_prolog.pl
@@ -1,11 +1,17 @@
 %consult('C:/Users/Benja/Desktop/Programación/WorkSpace/Tarea_prolog.pl').
+%swipl -s Tarea_prolog.pl
 
 sepparimpar(L, P, I).
 
-lista([]).
-list([]).
+%lista([],[],[]).
+%lista(Ps,Ps,[]).
+%lista([X|Xs],Ps,[X|Zs]):-
+%lista(Xs,Ys,Zs).
 
-lista([P|Ps]|[Y|Ys]):-list(X|Y),lista(Xs|Ys).
+len([],0,-1).
+len([L|R],Q,N):-
+    len(R,_,N1),
+    Q is L, N is N1+1.
 %------------------------------------------------------------------------------------------
 %todosrango(L, Min, Max).
 %------------------------------------------------------------------------------------------
